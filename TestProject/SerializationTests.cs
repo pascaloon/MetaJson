36a83fee56c3ec3cea97dc26940abd667f59bb0e
Pascal <pascalasselin@hotmail.com>
2020-11-21 11:12:06 -0500

    Added Test Project

---
+++ TestProject/SerializationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class SerializationTests
+    {
+        [Fact]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
+    }
+}
