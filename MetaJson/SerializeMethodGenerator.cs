6e64033efa06a30869a80d04e3e3bf10dae451e8
Pascal <pascalasselin@hotmail.com>
2020-11-29 01:55:46 -0500

    Added struct ref feature

--- MetaJson/SerializeMethodGenerator.cs
+++ MetaJson/SerializeMethodGenerator.cs
@@ -25,8 +25,12 @@ namespace MetaJson
                 return;
             _generatedSerializationMethods.Add(invocationTypeStr);
 
+            string refprefix = "";
+            if (ShouldUseRef(invocation.TypeArg))
+                refprefix = "ref ";
+
             const string SPC = "    ";
-            sb.Append($@"{SPC}{SPC}internal static string Serialize({invocationTypeStr} obj)");
+            sb.Append($@"{SPC}{SPC}internal static string Serialize({refprefix}{invocationTypeStr} obj)");
             sb.Append(@"
         {
 ");
@@ -175,11 +179,43 @@ namespace MetaJson
                 if (_generatedSerializationMethods.Contains(typeStr))
                     continue;
 
+                string refprefix = "";
+                if (ShouldUseRef(sc.Type))
+                    refprefix = "ref ";
+
                 // missing serialization method, create a stub one for intellisense
                 const string SPC = "    ";
-                sb.AppendLine($@"{SPC}{SPC}internal static string Serialize({typeStr} obj) {{ return String.Empty; }}");
+                sb.AppendLine($@"{SPC}{SPC}internal static string Serialize({refprefix}{typeStr} obj) {{ return String.Empty; }}");
 
             }
+        }
+
+        private bool ShouldUseRef(ITypeSymbol type)
+        {
+            if (!type.IsValueType)
+                return false;
+            switch (type.SpecialType)
+            {
+                case SpecialType.System_Boolean:
+                case SpecialType.System_Char:
+                case SpecialType.System_SByte:
+                case SpecialType.System_Byte:
+                case SpecialType.System_Int16:
+                case SpecialType.System_UInt16:
+                case SpecialType.System_Int32:
+                case SpecialType.System_UInt32:
+                case SpecialType.System_Int64:
+                case SpecialType.System_UInt64:
+                case SpecialType.System_Decimal:
+                case SpecialType.System_Single:
+                case SpecialType.System_Double:
+                case SpecialType.System_String:
+                case SpecialType.System_IntPtr:
+                case SpecialType.System_UIntPtr:
+                    return false;
+            }
+
+            return true;
         }
     }
 }
